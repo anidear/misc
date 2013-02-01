5e9ff8d058563268c4aea673ed4743fff36fdd9f
ptantiku <ptantiku@gmail.com>
2013-02-02 01:27:16 +0700

    snmpscan.sh: speed up the script

--- enumeration/snmpscan/snmpscan.sh
+++ enumeration/snmpscan/snmpscan.sh
@@ -31,46 +31,55 @@ else
 	grep_string='truehisp'
 fi
 
-echo "Scanning $iprange and saving result IP address to /tmp/snmpscanIP"
+echo "Scanning $iprange"
 #nmap -n -sS -p 161,162,10161,10162 $iprange | grep 'for' | sed  's/\(^.*(\)\|)//g' > /tmp/snmpscanIP
 #nmap -n -PS161,162,10161,10162 -sn $iprange | grep 'for' | cut -d' ' -f5 > /tmp/snmpscanIP
-nmap -n -sU -p 161 $iprange | grep 'for' | cut -d' ' -f5 > /tmp/snmpscanIP
+#iplist=$( nmap -n -sU -p 161 $iprange | grep -B3 -E 'open|filtered' | grep 'for' | cut -d' ' -f5 )
+iplist=$( nmap -n -sL $iprange | grep 'for' | cut -d' ' -f5 )
 echo "scan completed."
+
+
+snmp_query(){
+	local query=$1
+	local result=$(snmpwalk -r 0 -t 1 -c $community_string -v 1 $ip $query 2>/dev/null | grep -vi 'N/A' )
+	# sanitize 
+	result=$(echo -n $result | cut -d' ' -f4- | sed 's/[\r\n]//g' | sed 's/\([^"]\)$/\1"/' )
+	echo $result
+}
 
 echo "clean file /tmp/snmpscanout"
 >/tmp/snmpscanout
 
 echo "start enumerating all IP"
-for ip in $(cat /tmp/snmpscanIP); do
-	echo "[+] snmpwalk on $ip" 
+for ip in $iplist; do
 
-	snmpfile="/tmp/snmpscan$ip"
-	snmpwalk -r 2 -t 2 -c $community_string -v 1 $ip > $snmpfile
-	if [ -s $snmpfile ]; then 
-		echo "[*] scan is done. Result is stored in /tmp/snmpscan$ip"
+	echo "[*] snmp scan on $ip" 
 
-		# show grep result
-		data=$( grep -A 1 $grep_string $snmpfile | cut -d' ' -f4- )
-		if [ $(echo -n $data|wc -l) -eq 0 ]; then
-			data=$( grep -A 1 'iso.3.6.1.2.1.10.23.2.3.1' $snmpfile | grep -i 'STRING' | cut -d' ' -f4- )
-		fi
-		username=$( echo $data|cut -d' ' -f1 )
-		password=$( echo $data|cut -d' ' -f2 )
-
-		# show hw name
-		hwname=$( grep 'iso.3.6.1.2.1.1.5.0' $snmpfile | cut -d' ' -f4- | sed 's/\([^"]\)$/\1"/' )
-
-		# show hw version
-		hwversion=$( grep 'iso.3.6.1.2.1.1.1.0' $snmpfile | cut -d' ' -f4- | sed 's/\([^"]\)$/\1"/' )
-
-		echo -e "$ip\t$username\t$password\t$hwname\t$hwversion" >> /tmp/snmpscanout
-
-else
-		# if no result from the IP
-		echo "[-] no result from $ip"
-		rm $snmpfile
+	# check if change of IP class
+	if [ "$ipclass" != "$( echo -n $ip|cut -d'.' -f3 )" ]; then 
+		ipclass=$( echo -n $ip|cut -d'.' -f3 )
+		# adding a splitter between each IP class
+		echo '-------' >> /tmp/snmpscanout
 	fi
 
+	# query for username & password
+	username=$( snmp_query iso.3.6.1.2.1.10.23.2.3.1.5 )
+	if [ $username ]; then 
+		password=$( snmp_query iso.3.6.1.2.1.10.23.2.3.1.6 )
+
+		# query for hw name
+		hwname=$( snmp_query iso.3.6.1.2.1.1.5 )
+
+		# query for hw version
+		hwversion=$( snmp_query iso.3.6.1.2.1.1.1 )
+
+		# save result
+		echo -e "$ip\t$username\t$password\t$hwname\t$hwversion" >> /tmp/snmpscanout
+		echo -e "\e[01;32m[+] result from $ip is saved.\e[00m"
+	else
+		# no result
+		echo -e "\e[00;30m[-] no result.\e[00m"
+	fi
 done
 
 echo "finish."
